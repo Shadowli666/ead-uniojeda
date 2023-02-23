7446b22c6081d075d8a2d199b023c4804d07b6f2
Luis Felipe <31326261+Shadowli666@users.noreply.github.com>
2023-02-23 11:52:59 -0400

    first commit
    sql database

---
+++ ead.sql
@@ -0,0 +1,229 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 23-02-2023 a las 16:36:40
+-- Versión del servidor: 10.1.38-MariaDB
+-- Versión de PHP: 7.3.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `ead`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `carreras`
+--
+
+CREATE TABLE `carreras` (
+  `id` int(11) NOT NULL,
+  `carrera` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `datosestudiantes`
+--
+
+CREATE TABLE `datosestudiantes` (
+  `id` int(11) NOT NULL,
+  `nombres` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
+  `apellidos` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
+  `cedula` int(12) NOT NULL,
+  `procedencia` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
+  `carrera` int(11) NOT NULL,
+  `pais` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
+  `telefono` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
+  `telefono2` varchar(25) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `correo` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `nom_representante` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
+  `num_representante` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
+  `correo_representante` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
+  `estatus` varchar(10) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `materias`
+--
+
+CREATE TABLE `materias` (
+  `id` int(11) NOT NULL,
+  `nombre_materia` varchar(50) NOT NULL,
+  `carrera` int(11) NOT NULL,
+  `cod_materia` varchar(25) NOT NULL,
+  `id_profesor` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `notaestudiantes`
+--
+
+CREATE TABLE `notaestudiantes` (
+  `id` int(11) NOT NULL,
+  `id_lar` int(11) NOT NULL,
+  `id_estudiante` int(11) NOT NULL,
+  `id_materia` int(11) NOT NULL,
+  `nota_corte_uno` decimal(8,2) DEFAULT NULL,
+  `nota_corte_dos` decimal(8,2) DEFAULT NULL,
+  `nota_corte_tres` decimal(8,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `periodo`
+--
+
+CREATE TABLE `periodo` (
+  `id` int(11) NOT NULL,
+  `nombre_LAR` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `profesores`
+--
+
+CREATE TABLE `profesores` (
+  `id` int(11) NOT NULL,
+  `nombres` varchar(25) NOT NULL,
+  `apellidos` varchar(25) NOT NULL,
+  `correo` varchar(50) NOT NULL,
+  `telefono` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `carreras`
+--
+ALTER TABLE `carreras`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `datosestudiantes`
+--
+ALTER TABLE `datosestudiantes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `carrera` (`carrera`);
+
+--
+-- Indices de la tabla `materias`
+--
+ALTER TABLE `materias`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_profesor` (`id_profesor`),
+  ADD KEY `cod_materia` (`cod_materia`),
+  ADD KEY `carrera` (`carrera`);
+
+--
+-- Indices de la tabla `notaestudiantes`
+--
+ALTER TABLE `notaestudiantes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_estudiante` (`id_estudiante`),
+  ADD KEY `id_materia` (`id_materia`),
+  ADD KEY `id_lar` (`id_lar`);
+
+--
+-- Indices de la tabla `periodo`
+--
+ALTER TABLE `periodo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `profesores`
+--
+ALTER TABLE `profesores`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `carreras`
+--
+ALTER TABLE `carreras`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `datosestudiantes`
+--
+ALTER TABLE `datosestudiantes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `materias`
+--
+ALTER TABLE `materias`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `notaestudiantes`
+--
+ALTER TABLE `notaestudiantes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `periodo`
+--
+ALTER TABLE `periodo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `profesores`
+--
+ALTER TABLE `profesores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `datosestudiantes`
+--
+ALTER TABLE `datosestudiantes`
+  ADD CONSTRAINT `datosestudiantes_ibfk_1` FOREIGN KEY (`carrera`) REFERENCES `carreras` (`id`);
+
+--
+-- Filtros para la tabla `materias`
+--
+ALTER TABLE `materias`
+  ADD CONSTRAINT `materias_ibfk_1` FOREIGN KEY (`id_profesor`) REFERENCES `profesores` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `materias_ibfk_2` FOREIGN KEY (`carrera`) REFERENCES `carreras` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `notaestudiantes`
+--
+ALTER TABLE `notaestudiantes`
+  ADD CONSTRAINT `notaestudiantes_ibfk_1` FOREIGN KEY (`id_estudiante`) REFERENCES `datosestudiantes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `notaestudiantes_ibfk_2` FOREIGN KEY (`id_lar`) REFERENCES `periodo` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `notaestudiantes_ibfk_3` FOREIGN KEY (`id_materia`) REFERENCES `materias` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
